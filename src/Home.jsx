5354652bfaee4adcabdfebd90fc49819ee1e3f6f
unknown <ziaul935@gmail.com>
2020-10-14 01:53:11 +0600

    home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,33 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
+import wed from "../src/imgs/img1.svg"
 
 const Home =()=>{
     return(
         <>
-        <h1 > welcome home </h1>
+        <section id="header" className="d-flex align-items-center">
+            <div className="container-fluid nav_bg">
+                <div className="row">
+                    <div className="col-10 mx-auto">
+                        <div className="row">
+                        <div className="col-md-6 pt-5 pt-lg-0  order-2 order-lg-1 d-flex  justify-content-center flex-column ">
+                            <h1> 
+                                Grow your business with <strong className="brand-name"> Awesome11</strong>
+                            </h1>
+                            <h2 className ="my-3">we are the team of telented developer making websites</h2>
+                            <div className="mt-3">
+                            <NavLink to="/service"  className="btn-get-started"> Getting start </NavLink>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 order-1 order-lg-2 header-img">
+                            <img src={wed} className="img-fluid animated"/>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
         </>
     )
 }
