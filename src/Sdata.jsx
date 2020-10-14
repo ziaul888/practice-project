0ac41a1aea405aab1a80adcd423ae5dc48577403
unknown <ziaul935@gmail.com>
2020-10-14 06:00:41 +0600

    service component

---
+++ src/Sdata.jsx
@@ -0,0 +1,35 @@
+import web from "./imgs/f.jpg"
+import app from "./imgs/w.jpg"
+import android from "./imgs/Work.png"
+import digital from "./imgs/lar8.png"
+import marketing from "./imgs/x.jpg"
+import software from "./imgs/f.jpg"
+
+
+const Sdata =[
+    {
+        imgsrc:web,
+        title:"web development",
+    },
+    {
+        imgsrc:app,
+        title:"app development",
+    },
+    {
+        imgsrc:software,
+        title:"software development",
+    },
+    {
+        imgsrc:digital,
+        title:"digital marketing",
+    },
+    {
+        imgsrc:marketing,
+        title:"business marketing",
+    },
+    {
+        imgsrc:android,
+        title:"android development",
+    }
+]
+export default Sdata
